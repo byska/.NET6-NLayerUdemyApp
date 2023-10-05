63a171a8ae41b1bb7cca53a2037785766b6ce8e1
Beste Yasak <beste.yasak@bilgeadamboost.com>
2023-10-05 18:51:40 +0300

    //dtos

--- NLayer.Repository/Seeds/CategorySeed.cs
+++ NLayer.Repository/Seeds/CategorySeed.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using NLayer.Core;
+using NLayer.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +13,7 @@ namespace NLayer.Repository.Seeds
     {
         public void Configure(EntityTypeBuilder<Category> builder)
         {
-            builder.HasData(new Category { Id = 1, Name = "Kalemler" },
-                new Category { Id = 2, Name = "Kitap" },
-                new Category { Id = 3, Name = "Defter" });
+            builder.HasData(new Category { Id = 1, Name = "Kalemler" }, new Category { Id = 2, Name = "Kitap" }, new Category { Id = 3, Name = "Defter" });
 
         }
     }
--- NLayer.Repository/Seeds/ProductSeed.cs
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using NLayer.Core;
+using NLayer.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,17 +10,17 @@ using System.Threading.Tasks;
 
 namespace NLayer.Repository.Seeds
 {
-    public class ProductSeed : IEntityTypeConfiguration<Product>
+    public class ProductSeed : IEntityTypeConfiguration<Category>
     {
-        public void Configure(EntityTypeBuilder<Product> builder)
+        public void Configure(EntityTypeBuilder<Category> builder)
         {
             builder.HasData(new Product { Id = 1, Name = "Kalem 1", CategoryId = 1, Price = 100, Stock = 20, CreatedDate = DateTime.Now },
-                new Product { Id = 2, Name = "Kalem 2", CategoryId = 1, Price = 200, Stock = 30, CreatedDate = DateTime.Now },
-                new Product { Id = 3, Name = "Kalem 3", CategoryId = 1, Price = 600, Stock = 60, CreatedDate = DateTime.Now },
-                new Product { Id = 4, Name = "Kalem 3", CategoryId = 1, Price = 600, Stock = 60, CreatedDate = DateTime.Now },
+                new Product { Id = 1, Name = "Kalem 2", CategoryId = 1, Price = 200, Stock = 30, CreatedDate = DateTime.Now },
+                new Product { Id = 1, Name = "Kalem 3", CategoryId = 1, Price = 600, Stock = 60, CreatedDate = DateTime.Now },
+                new Product { Id = 1, Name = "Kalem 3", CategoryId = 1, Price = 600, Stock = 60, CreatedDate = DateTime.Now },
 
-                new Product { Id = 5, Name = "Kitap 1", CategoryId = 2, Price = 600, Stock = 60, CreatedDate = DateTime.Now },
-                new Product { Id = 6, Name = "Kitap 2", CategoryId = 2, Price = 6600, Stock = 320, CreatedDate = DateTime.Now }
+                new Product { Id = 1, Name = "Kitap 1", CategoryId = 2, Price = 600, Stock = 60, CreatedDate = DateTime.Now },
+                new Product { Id = 1, Name = "Kitap 2", CategoryId = 2, Price = 6600, Stock = 320, CreatedDate = DateTime.Now }
 
                 );
         }
