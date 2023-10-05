effbf798dc055baa4d5a296bbb26ddbdb6e18ba8
Beste Yasak <beyasak@hotmail.com>
2023-10-05 16:05:46 +0300

    .

--- NLayer.Repository/Seeds/ProductSeed.cs
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -14,12 +14,12 @@ namespace NLayer.Repository.Seeds
         public void Configure(EntityTypeBuilder<Category> builder)
         {
             builder.HasData(new Product { Id = 1, Name = "Kalem 1", CategoryId = 1, Price = 100, Stock = 20, CreatedDate = DateTime.Now },
-                new Product { Id = 1, Name = "Kalem 2", CategoryId = 1, Price = 200, Stock = 30, CreatedDate = DateTime.Now },
-                new Product { Id = 1, Name = "Kalem 3", CategoryId = 1, Price = 600, Stock = 60, CreatedDate = DateTime.Now },
-                new Product { Id = 1, Name = "Kalem 3", CategoryId = 1, Price = 600, Stock = 60, CreatedDate = DateTime.Now },
+                new Product { Id = 2, Name = "Kalem 2", CategoryId = 1, Price = 200, Stock = 30, CreatedDate = DateTime.Now },
+                new Product { Id = 3, Name = "Kalem 3", CategoryId = 1, Price = 600, Stock = 60, CreatedDate = DateTime.Now },
+                new Product { Id = 4, Name = "Kalem 3", CategoryId = 1, Price = 600, Stock = 60, CreatedDate = DateTime.Now },
 
-                new Product { Id = 1, Name = "Kitap 1", CategoryId = 2, Price = 600, Stock = 60, CreatedDate = DateTime.Now },
-                new Product { Id = 1, Name = "Kitap 2", CategoryId = 2, Price = 6600, Stock = 320, CreatedDate = DateTime.Now }
+                new Product { Id = 5, Name = "Kitap 1", CategoryId = 2, Price = 600, Stock = 60, CreatedDate = DateTime.Now },
+                new Product { Id = 6, Name = "Kitap 2", CategoryId = 2, Price = 6600, Stock = 320, CreatedDate = DateTime.Now }
 
                 );
         }
